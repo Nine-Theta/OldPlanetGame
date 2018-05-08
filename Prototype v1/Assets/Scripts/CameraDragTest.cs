03546d590c24dd44727eb94729d0ee00f8eda0bd
David <davidheetebrij@gmail.com>
2018-05-08 16:02:55 +0200

    Added a mudball which you can view from different angles

---
+++ Prototype v1/Assets/Scripts/CameraDragTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDragTest : MonoBehaviour{
+
+    private Transform _focusTransform;
+    private Rigidbody _focusbody;
+    
+	private void Start (){
+
+        _focusbody = GetComponentInParent<Rigidbody>();
+        _focusTransform = _focusbody.GetComponent<Transform>();
+	}
+	
+	private void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            //_focusTransform.rotation = Quaternion.Euler(Input.GetAxis("Mouse Y") + _focusTransform.rotation.eulerAngles.x, Input.GetAxis("Mouse X") + _focusTransform.rotation.eulerAngles.y, 0);
+            _focusbody.AddTorque(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
+        }
+	}
+}
---
+++ Prototype v1/Assets/Scripts/OrbitScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialVelocity : MonoBehaviour {
+
+    public Rigidbody _focusBody;
+    public float speed = 0.1f;
+
+	private void Awake () {
+        //_focusBody = gameObject.GetComponent<Rigidbody>();
+
+        //GetComponent<Rigidbody>().AddTorque(0, 1, 0);
+        //_focusBody.AddTorque(0,1,0);
+        //_focusBody.
+    }
+    private void Update()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0, speed, 0));
+    }
+}
