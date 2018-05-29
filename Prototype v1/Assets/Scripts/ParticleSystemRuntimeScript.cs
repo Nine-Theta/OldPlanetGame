39766371790ac98a97b31d01dd0a1843d3f72a91
CHRISTIAN\Christian <433910@student.saxion.nl>
2018-05-29 16:21:21 +0200

    Quick test for ParticleSystem accessing Script

---
+++ Prototype v1/Assets/Scripts/ParticleSystemRuntimeScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemRuntimeScript : MonoBehaviour
+{
+    private ParticleSystem _system;
+
+    private void Start()
+    {
+        _system = GetComponent<ParticleSystem>();
+    }
+
+    public void SetLooping(bool looping)
+    {
+        //ParticleSystem.MainModule sysMain = _system.main;
+        //sysMain.loop = looping;
+#pragma warning disable CS0618 // Type or member is obsolete
+        _system.loop = looping;
+#pragma warning restore CS0618 // Type or member is obsolete
+    }
+}
