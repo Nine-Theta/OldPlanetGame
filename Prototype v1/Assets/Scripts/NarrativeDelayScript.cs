8bd236bef39a8567cfb2adc239b66d66404bb80d
CHRISTIAN\Christian <433910@student.saxion.nl>
2018-06-27 11:08:16 +0200

    Narrative delay. You cannot skip through it now. It smells like pipes

---
+++ Prototype v1/Assets/Scripts/NarrativeDelayScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NarrativeDelayScript : MonoBehaviour {
+    [SerializeField] private float _delay = 0.5f;
+    private Button _button;
+
+	private void Start () {
+        _button = GetComponentInChildren<Button>();
+        _button.interactable = false;
+	}
+	
+	private void Update () {
+        _delay -= Time.deltaTime;
+        if(_delay <= 0.0f)
+        {
+            _button.interactable = true;
+            this.enabled = false;
+        }
+	}
+}
