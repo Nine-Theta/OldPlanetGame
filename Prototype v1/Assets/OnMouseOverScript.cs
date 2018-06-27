fe63db805e9c5ac331bf0a5d50a9caf4b69ec063
CHRISTIAN\Christian <433910@student.saxion.nl>
2018-06-27 21:26:37 +0200

    Added an OnMouseOverScript

---
+++ Prototype v1/Assets/OnMouseOverScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnMouseOverScript : MonoBehaviour
+{
+    [SerializeField] private CustomEvent OnMouseOverEvent;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnMouseOver()
+    {
+        OnMouseOverEvent.Invoke();
+    }
+}
